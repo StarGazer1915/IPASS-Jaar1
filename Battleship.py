c2f4cbf388cd5ca71f6137497c708badfc268e06
StarGazer1915 <74311615+StarGazer1915@users.noreply.github.com>
2021-06-07 11:02:23 +0200

    Setup battleship game infrastructure (Imports, Global vars, Functions, )

---
+++ Battleship.py
@@ -0,0 +1,61 @@
+# ========== STUDENT ========== #
+#   Naam:       Justin Klein
+#   Klas:       V1B
+#   Nummer:     1707815
+#   Project:    IPASS
+# ============================= #
+
+
+# ============ IMPORTS ============ #
+import random
+
+
+# ============ GLOBALS ============ #
+battlefield = [[]]
+field_size = 10
+num_ships = 8
+ammo = 50
+ships_destroyed = 0
+ship_positions = [[]]
+row_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+# ============ FUNCTIONS ============ #
+def checkValidShipPlacement(start_row, end_row, start_col, end_col):
+    pass
+
+
+def placeShip(row, col, direction, length):
+    pass
+
+
+def createField():
+    pass
+
+
+def showField():
+    pass
+
+
+def CheckShellChoice():
+    pass
+
+
+def checkShipDestroyed(row, col):
+    pass
+
+
+def fireShell():
+    pass
+
+
+def checkGameOver():
+    pass
+
+
+def runGame():
+    pass
+
+
+# ============ EXECUTION ============ #
+runGame()

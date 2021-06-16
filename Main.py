abae0d271650f246b94560f69e00739875f65d11
StarGazer1915 <74311615+StarGazer1915@users.noreply.github.com>
2021-06-16 14:17:03 +0200

    MAIN: Added student information comments

--- Main.py
+++ Main.py
@@ -1,3 +1,13 @@
+# ============ STUDENT ============ #
+#   Naam:       Justin Klein
+#   Nummer:     1707815
+#   Project:    IPASS
+# ================================= #
+
+# Run this file to start the game.
+
+# ============ IMPORTS ============ #
 from gui.Dashboard import ShowBoardGame
+# ================================= #
 
 ShowBoardGame()
